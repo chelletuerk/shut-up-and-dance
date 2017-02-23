5aac72cb5a5ffcea27267e0dce4127baeedd5385
chelle tuerk <michelletuerk@gmail.com>
2017-02-23 16:53:18 -0700

    major css redo--all the box shadows

--- app/actions/index.js
+++ app/actions/index.js
@@ -17,22 +17,36 @@ export const displaySearchedArtist = (query, payload) => {
 
  export const fetchArtist = (query) => {
   const baseUrl = 'https://api.spotify.com/'
-  const search = `v1/search?q=%20artist:${query}&type=album`
-  // const search = `v1/audio-features/06AKEBrKUckW0KREUWRnvT`
+  const search = `v1/search?q=%20artist:${query}&type=track`
   return (dispatch) => {
-    // const headers = {'Authorization': 'Bearer ' + window.spotifyAccessToken }
-    fetch(`${baseUrl}${search}`)//, {headers})
+    const headers = {'Authorization': 'Bearer ' + window.spotifyAccessToken }
+    fetch(`${baseUrl}${search}`, {headers})
       .then(response => response.json())
       .then((json) => {
-
-        // console.log('hey', json);
         dispatch(displaySearchedArtist(query, json))
-        // const artistId = json.body.artistId
-        // fetch(`${baseUrl}${artistId}`)
-        //   .then(=>
-        //     dispatch(displaySearchedArtist(query, json))
-        //   )
       })
       .catch(err => 'err')
   }
 }
+
+//  export const fakeArtist = (query) => {
+//   const baseUrl = 'https://api.spotify.com/'
+//   const search = `v1/search?q=%20artist:${query}&type=album`
+//   // const search = `v1/audio-features/06AKEBrKUckW0KREUWRnvT`
+//   return (dispatch) => {
+//     // const headers = {'Authorization': 'Bearer ' + window.spotifyAccessToken }
+//     fetch(`${baseUrl}${search}`)//, {headers})
+//       .then(response => response.json())
+//       .then((json) => {
+//
+//         // console.log('hey', json);
+//         dispatch(displaySearchedArtist(query, json))
+//         // const artistId = json.body.artistId
+//         // fetch(`${baseUrl}${artistId}`)
+//         //   .then(=>
+//         //     dispatch(displaySearchedArtist(query, json))
+//         //   )
+//       })
+//       .catch(err => 'err')
+//   }
+// }
