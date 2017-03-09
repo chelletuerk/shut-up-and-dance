663b00f60501f63e7492c053f0cf3b3f3a10d9c0
chelle tuerk <michelletuerk@gmail.com>
2017-03-09 09:39:49 -0700

    update readme

---
+++ test/mockData.js
@@ -0,0 +1,16 @@
+
+    const item1 = {id: 1}
+    const item2 = {id: 2}
+    const item3 = {id: 3}
+    const item4 = {id: 4}
+
+   const artist1 = {name: 'Prince', items: [item1, item2, item3, item4]}
+   const artist2 = {name: 'Run DMC', items: [item1, item2, item3, item4]}
+   const artist3 = {name: 'Blackalicious', items: [item1, item2, item3, item4]}
+  const artist4 = {name: 'Beastie Boys', items: [item1, item2, item3, item4]}
+  const artist5 = {name: 'People under the stairs', items: [item1, item2, item3, item4]}
+
+
+    export const mockArtists = {
+      artists: [artist1, artist2, artist3, artist4, artist5]
+    };
