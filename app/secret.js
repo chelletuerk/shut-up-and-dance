508457c68460931c300d08a77b60bde6de18fd6a
chelle tuerk <michelletuerk@gmail.com>
2017-03-11 11:22:36 -0700

    refresh key to avoid exposure

--- app/secret.js
+++ app/secret.js
@@ -2,7 +2,7 @@ var keys = function () {
   return (
     {
       client_id: 'e57daeab4cd24459a924105142807fa9',
-      client_secret:  '1ae09ed5e49048418adb956e9bc202a4'
+      client_secret:  '52f7b31b02494e3491386c1c6cd78433'
     }
   )
 
